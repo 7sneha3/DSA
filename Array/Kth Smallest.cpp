dfe760ad900ec66a8bec4c2772be950b1b6239ef
Sneha Sakhare <154829884+7sneha3@users.noreply.github.com>
2025-02-28 12:33:37 +0530

    Update Kth Smallest.cpp

--- Array/Kth Smallest.cpp
+++ Array/Kth Smallest.cpp
@@ -21,6 +21,14 @@ Constraints:
 */
 
 int kthSmallest(vector<int> &arr, int k) {
-    sort(arr.begin(), arr.end());
+    // sort(arr.begin(), arr.end());
+    for(int i=0; i<arr.size(); i++){
+        for(int j=i+1; j<arr.size(); j++){
+            if(arr[i] > arr[j]){
+                swap(arr[i], arr[j]);
+            }
+        }
+    }
+    
     return arr[k-1];
 }
