6f8b7d8cdff82a84471bbc6e3d59f27e720fbaa0
Sneha Sakhare <154829884+7sneha3@users.noreply.github.com>
2025-04-22 21:23:18 +0530

    Update Reverse a string.cpp

--- Recursion/Reverse a string.cpp
+++ Recursion/Reverse a string.cpp
@@ -18,3 +18,21 @@ int main(){
     string s = "helloji";
     reverse(s, s.size());
 }
+
+//Another logic
+void reverse(string &s, int st, int e){
+    if(st>=e) return;
+    
+    swap(s[st], s[e]);
+    // cout<<s; //3times output karan swap kela ki lagech print hotae
+    // cout<<s[st]<<" "; //wrong output
+
+    reverse(s, st+1, e-1);   
+    // cout<<s; //3times output karan swap kela ki lagech print hotae   
+}
+
+int main(){
+    string s = "helloji";
+    reverse(s, 0, s.size()-1);
+    cout<<s;
+}
