59d05816be95fcaffa790328a97fb561f2a0e2cb
Sneha Sakhare <154829884+7sneha3@users.noreply.github.com>
2025-05-24 09:15:34 +0530

    Create Find Words Containing character.cpp

---
+++ Array/Find Words Containing character.cpp
@@ -0,0 +1,60 @@
+/*
+You are given a 0-indexed array of strings words and a character x.
+
+Return an array of indices representing the words that contain the character x.
+
+Note that the returned array may be in any order.
+
+ 
+
+Example 1:
+
+Input: words = ["leet","code"], x = "e"
+Output: [0,1]
+Explanation: "e" occurs in both words: "leet", and "code". Hence, we return indices 0 and 1.
+Example 2:
+
+Input: words = ["abc","bcd","aaaa","cbc"], x = "a"
+Output: [0,2]
+Explanation: "a" occurs in "abc", and "aaaa". Hence, we return indices 0 and 2.
+Example 3:
+
+Input: words = ["abc","bcd","aaaa","cbc"], x = "z"
+Output: []
+Explanation: "z" does not occur in any of the words. Hence, we return an empty array.
+ 
+
+Constraints:
+
+1 <= words.length <= 50
+1 <= words[i].length <= 50
+x is a lowercase English letter.
+words[i] consists only of lowercase English letters.
+*/
+
+vector<int> findWordsContaining(vector<string>& words, char x) {
+        /*
+        //tc O(n^2)
+        vector<int>str_idx;
+        for(int i=0; i<words.size(); i++){
+            for(char c : words[i]){
+                if(c == x){
+                    str_idx.push_back(i);
+                    break; //eka word madhey kiti pn wela toch char yeu de, ekda bhetla ki break
+                }
+            }
+        }
+        return str_idx;
+        */
+
+        //tc O(n)
+        vector<int>str_idx;
+        int i=0;
+        for(string&s: words){
+            if(find(s.begin(), s.end(), x) != s.end()){
+                str_idx.push_back(i);
+            }
+            i++;
+        }
+        return str_idx;
+    }
